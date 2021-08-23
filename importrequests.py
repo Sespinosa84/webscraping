dc770fc5b701801c095e0fe3ad345ab8338260d4
Sergio Espinosa <sergioespinosa@Sergios-MacBook-Pro.local>
2021-08-22 20:22:31 -0500

    beatiful methods

--- importrequests.py
+++ importrequests.py
@@ -2,9 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
+soup = BeautifulSoup(res.text, 'html.parser')
 
-
-print(res.text)
-
-
+print(soup.find_all('div'))
   
