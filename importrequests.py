316c48564367cb13ea53182e1f3fb29c7452e804
Sergio Espinosa <sergioespinosa@Sergios-MacBook-Pro.local>
2021-08-22 20:46:56 -0500

    title & links

--- importrequests.py
+++ importrequests.py
@@ -7,7 +7,16 @@ soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.storylink') # grap the links
 votes = soup.select('.score') #grap the votes 
 
+def create_custom_hn(links, votes):
+	hn = []
+	for idx, item in enumerate(links):
+		title = links[idx].getText()
+		#hn.append(title)
+		href = links[idx].get('href', None)
+		hn.append({'title', title,'link', href})
+
+	return hn 
 
 
-
+print(create_custom_hn(links, votes))
   
