90718b63619b5cc13f2b46c87ce306028ca47a0f
Sergio Espinosa <sergioespinosa@Sergios-MacBook-Pro.local>
2021-08-23 20:46:17 -0500

    include second page

--- importrequests.py
+++ importrequests.py
@@ -3,10 +3,18 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
 
 links = soup.select('.storylink') # grap the links
 subtext = soup.select('.subtext') #grap the votes 
+
+links2 = soup2.select('.storylink') # grap the links
+subtext2 = soup2.select('.subtext') #grap the votes 
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
 
 def sort_stories_by_votes(hnlist):
 	return sorted(hnlist, key = lambda k:k['votes'], reverse=True)
@@ -26,5 +34,5 @@ def create_custom_hn(links, subtext):
 	return sort_stories_by_votes(hn) 
 
 
-pprint.pprint(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
   
