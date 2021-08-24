7eb0a6dff01accd1bd1fcadba669475ea0a2b73d
Sergio Espinosa <sergioespinosa@Sergios-MacBook-Pro.local>
2021-08-23 19:23:35 -0500

    exclude those that doesn't have votes

--- importrequests.py
+++ importrequests.py
@@ -5,22 +5,24 @@ res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 
 links = soup.select('.storylink') # grap the links
-votes = soup.select('.score') #grap the votes 
+subtext = soup.select('.subtext') #grap the votes 
 
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subtext):
 	hn = []
 	for idx, item in enumerate(links):
 		title = links[idx].getText()
 		#hn.append(title)
 		href = links[idx].get('href', None)
-		score = int(votes[idx].getText().replace(' points', ''))
-		print(score)
-		hn.append({'title', title,'link', href})
+		vote = subtext[idx].select('.score')
+		if len(vote):
+			score = int(vote[0].getText().replace(' points', ''))
+			print(score)
+			hn.append({'title', title,'link', href})
 
 
 
 	return hn 
 
 
-print(create_custom_hn(links, votes))
+print(create_custom_hn(links, subtext))
   
